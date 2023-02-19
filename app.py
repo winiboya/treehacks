aba4fefe5172cc6e0a3a1c1fe09735f80872ee60
winiboya <w>
2023-02-19 09:19:28 -0800

    end of hackathon

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = "key"
 Session(app)
 
-API_KEY = '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'
+API_KEY = 'apikey'
 openai.api_key = API_KEY
 
 model = 'text-davinci-003'
+history = []
 
 # openai.api_key = os.environ['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']
 
@@ -42,6 +43,12 @@ def login_required(f):
 @login_required
 def index():
     cursor = db.cursor()
+    # cursor.execute("SELECT account FROM users WHERE id = ?", [session["user_id"]])
+    # account = cursor.fetchone()[0]
+
+    # if account == "student":
+    global history
+    history = []
     if request.method == "GET":
         cursor.execute("SELECT topics.course_id,courses.name, topics.name, description FROM ((courses INNER JOIN courses_users  ON courses.id = courses_users.course_id) INNER JOIN topics ON topics.course_id = courses.id) WHERE courses_users.user_id = ?", [session["user_id"]])
         courses = cursor.fetchall()
@@ -60,17 +67,129 @@ def index():
             new_dict["course_name"] = course[1]
             new_dict["topic_name"] = course[2]
             new_dict["description"] = course[3]
- 
+
             course_dict.append(new_dict)
 
         
-        print(course_dict)
         cursor.close()
         return render_template("index.html", course_dict=course_dict, course_names=course_names)
     else:
         course = request.form.get("course")
+        print(course)
 
-        cursor.execute("SELECT topics.course_id,courses.name, topics.name, description FROM ((courses INNER JOIN courses_users  ON courses.id = courses_users.course_id) INNER JOIN topics ON topics.course_id = courses.id) WHERE courses_users.user_id = ? AND courses.name = ?", [session["user_id"], course])
+        if(course):
+            cursor.execute("SELECT topics.course_id,courses.name, topics.name, description FROM ((courses INNER JOIN courses_users  ON courses.id = courses_users.course_id) INNER JOIN topics ON topics.course_id = courses.id) WHERE courses_users.user_id = ? AND courses.name = ?", [session["user_id"], course])
+            courses = cursor.fetchall()
+
+            cursor.execute("SELECT courses.name FROM courses INNER JOIN courses_users ON courses.id = courses_users.course_id WHERE courses_users.user_id = ?", [session["user_id"]])
+            course_name_tuple = cursor.fetchall()
+            course_names = []
+            for course_name in course_name_tuple:
+                course_names.append(course_name[0])
+            course_names = list(course_names)
+
+
+            course_dict = []
+            for course in courses:
+                new_dict = {}
+                new_dict["course_name"] = course[1]
+                new_dict["topic_name"] = course[2]
+                new_dict["description"] = course[3]
+    
+                course_dict.append(new_dict)
+
+            
+            print(course_dict)
+            cursor.close()
+            return render_template("index.html", course_dict=course_dict, course_names=course_names)
+        new_course = request.form.get("course-code")
+        if (new_course):
+            cursor.execute("SELECT id FROM courses WHERE course_code = ?", [new_course])
+            course_id = cursor.fetchone()[0]
+            cursor.execute("INSERT INTO courses_users (course_id, user_id) VALUES (?, ?)", [course_id, session["user_id"]])
+            db.commit()
+            cursor.close()
+            return redirect("/")
+            
+  
+
+@app.route("/topic", methods=["GET", "POST"])
+@login_required
+def topic():
+    global history
+    print(history)
+    cursor = db.cursor()
+    if request.method == "GET":
+        history = []
+        cursor.close()
+        return render_template("topic.html")
+    else:
+        if (isinstance(request.form.get("topic"), str)):
+            global topic 
+            topic = request.form.get("topic")
+        cursor.execute("SELECT topics.course_id, topics.name, topics.description, topics.question1, topics.question2, topics.question3 FROM ((courses INNER JOIN courses_users  ON courses.id = courses_users.course_id) INNER JOIN topics ON topics.course_id = courses.id) WHERE courses_users.user_id = ? AND topics.name = ?", [session["user_id"], topic])
+        topic_info = cursor.fetchall()
+        questions = [topic_info[0][3], topic_info[0][4], topic_info[0][5]]
+
+
+    
+        print("userid", session["user_id"])
+        cursor.execute("SELECT grade FROM users WHERE id = ?", [session["user_id"]])
+        # print(cursor.fetchone()[0])
+        grade = cursor.fetchone()[0]
+
+        prefix_dict = {
+            "question": f"Pretend I am {grade} in order to answer age appropriately. Please describe it so it is easy to understand for my age.A nswer in one or two paragraphs. ",
+            "understanding": f"Pretend I am {grade} in order to answer age appropriately. I am having trouble understanding a concept in class.  Please give an explanation that is easy to understand for my age. Answer in one or two paragraphs. ",
+            "show": f"Pretend I am {grade} in order to answer age appropriately. I would like you to show me a complex from class. Please describe it so it is easy to understand for my age. Answer in one or two paragraphs. ",
+            "moreabout": f"Pretend I am {grade} in order to answer age appropriately. I would like to know more about something I'm learning in class. Please give me some information that would not be considered basic. Answer in one or two paragraphs. Please describe it so it is easy to understand for my age. ",
+            "example": f"Pretend I am {grade} in order to answer age appropriately I would like an example for something I'm learning in class. Make sure it would be understandable for my age. "
+        }
+
+
+        if (request.form.get("understanding")):
+            question = "Please help me understand " + (request.form.get("understanding") + ".")
+            prefix = prefix_dict["understanding"]
+            response = output(question, prefix+question)
+
+        elif (request.form.get("show")):
+            question = ("Show me how to " + request.form.get("show"))
+            prefix = prefix_dict["show"]
+            output(question, prefix+question)
+        elif (request.form.get("moreabout")):
+            question = "Tell me more about " + (request.form.get("moreabout"))
+            prefix = prefix_dict["moreabout"]
+            output(question, prefix+question)
+
+        elif (request.form.get("example")):
+            question = "Give me an example of " + (request.form.get("example"))
+            prefix =  prefix_dict["example"]
+            output(question, prefix+question)
+
+        elif (request.form.get("question")):
+            question = (request.form.get("question"))
+            prefix = prefix_dict["question"]
+            output(question, prefix+question)
+ 
+        print("response",history)
+
+        cursor.close()
+        return render_template("topic.html", topic=topic, questions=questions, history=history)
+
+
+@app.route("/teacher", methods=["GET", "POST"])
+@login_required
+def teacher():
+    cursor = db.cursor()
+    cursor.execute("SELECT account FROM users WHERE id = ?", [session["user_id"]])
+    account = cursor.fetchone()[0]
+    if account != "teacher":
+        return "Error: must be a teacher"
+    if request.method=="GET":
+        course = request.form.get("course")
+        print(course)
+
+        cursor.execute("SELECT topics.course_id,courses.name, topics.name, description FROM ((courses INNER JOIN courses_users  ON courses.id = courses_users.course_id) INNER JOIN topics ON topics.course_id = courses.id) WHERE courses_users.user_id = ?", [session["user_id"]])
         courses = cursor.fetchall()
 
         cursor.execute("SELECT courses.name FROM courses INNER JOIN courses_users ON courses.id = courses_users.course_id WHERE courses_users.user_id = ?", [session["user_id"]])
@@ -87,74 +206,13 @@ def index():
             new_dict["course_name"] = course[1]
             new_dict["topic_name"] = course[2]
             new_dict["description"] = course[3]
- 
+
             course_dict.append(new_dict)
 
-        
-        print(course_dict)
-        cursor.close()
-        return render_template("index.html", course_dict=course_dict, course_names=course_names) 
 
-@app.route("/topic", methods=["GET", "POST"])
-@login_required
-def topic():
-    cursor = db.cursor()
-    if request.method == "GET":
-        cursor.close()
-        return render_template("topic.html")
+        return render_template("teacher.html", course_names=course_names, course_dict = course_dict)
     else:
-        if (isinstance(request.form.get("topic"), str)):
-            global topic 
-            topic = request.form.get("topic")
-        cursor.execute("SELECT topics.course_id, topics.name, topics.description, topics.question1, topics.question2, topics.question3 FROM ((courses INNER JOIN courses_users  ON courses.id = courses_users.course_id) INNER JOIN topics ON topics.course_id = courses.id) WHERE courses_users.user_id = ? AND topics.name = ?", [session["user_id"], topic])
-        topic_info = cursor.fetchall()
-        questions = [topic_info[0][3], topic_info[0][4], topic_info[0][5]]
-
-
-    
-
-        cursor.execute("SELECT grade FROM users WHERE id = ?", [session["user_id"]])
-        grade = cursor.fetchone()[0]
-        print(grade)
-
-        prefix_dict = {
-            "question": f"Pretend I am {grade}. Please describe it so it is easy to understand for my age.A nswer in one or two paragraphs. If you cannot generate a response, say \"Hmm, sounds like a good question for your teacher.\"",
-            "understanding": f"Pretend I am {grade}. I am having trouble understanding a concept in class.  Please give an explanation that is easy to understand for my age. Answer in one or two paragraphs. If you cannot generate a response, say \"Hmm, sounds like a good question for your teacher.\" Explain ",
-            "show": f"Pretend I am {grade}. I would like you to show me a complex from class. Please describe it so it is easy to understand for my age. Answer in one or two paragraphs. If you cannot generate a response, say \"Hmm, sounds like a good question for your teacher.\" Show me how to ",
-            "moreabout": f"Pretend I am {grade}. I would like to know more about something I'm learning in class. Please give me some information that would not be considered basic. Answer in one or two paragraphs. Please describe it so it is easy to understand for my age. If you cannot generate a response, say \"Hmm, sounds like a good question for your teacher.\" Tell me more about ",
-            "example": f"Pretend I am {grade}. I would like an example for something I'm learning in class. Make sure it would be understandable for my age. If you cannot generate a response, say \"Hmm, sounds like a good question for your teacher.\" Give me an example of "
-        }
-
-        if (request.form.get("question")):
-            question = (request.form.get("question"))
-            prefix = prefix_dict["question"]
-            response = call_api(prefix+question)
-
-        elif (request.form.get("understanding")):
-            question = (request.form.get("understanding"))
-            prefix = prefix_dict["understanding"]
-            response = call_api(prefix+question)
-
-        elif (request.form.get("show")):
-            question = (request.form.get("show"))
-            prefix = prefix_dict["show"]
-            response = call_api(prefix+question)
-        elif (request.form.get("moreabout")):
-            question = (request.form.get("moreabout"))
-            prefix = prefix_dict["moreabout"]
-            response = call_api(prefix+question)
-
-        elif (request.form.get("example")):
-            question = (request.form.get("example"))
-            prefix = prefix_dict["example"]
-            response = call_api(prefix+question)
-        else:
-            response = None
-        print(response)
-
-        cursor.close()
-        return render_template("topic.html", topic=topic, questions=questions, response=response)
-
+        return render_template("teacher.html")
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -213,8 +271,9 @@ def signup():
 
 
         # Remember which user has logged in
-        session["user_id"] = rows[0][1]
-        print(session["user_id"])
+        session["user_id"] = rows[0][0]
+        # print(rows)
+        # print(session["user_id"])
 
         cursor.close()
         # Redirect user to home page
@@ -276,9 +335,21 @@ def logout():
 
 
 def call_api(prompt):
-    response = openai.Completion.create(prompt=prompt, model=model, max_tokens=1000, n=1, temperature=0.9, stop=["--"])
+    response = openai.Completion.create(prompt=prompt, model=model, max_tokens=1000, n=1, temperature=0.6, stop=["--"])
     for result in response.choices:
         return result.text
+
+
+
+def output(input, prompt):
+    global history
+    # s = list(sum(history, ()))
+    # s.append(input)
+    # inp = ' '.join(s)
+    output = call_api(prompt)
+    # output = output[1:]
+    new_dict = {"input": input, "output": output}
+    history.append(new_dict)
 
 
 # # openai explanation generator
